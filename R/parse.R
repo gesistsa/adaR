eb72d6c210213d459a9cad8e2bdf2d2abb7e3b14
chainsawriot <chainsawtiney@gmail.com>
2023-09-22 11:50:01 +0200

    Fix #5

--- R/parse.R
+++ R/parse.R
@@ -1,11 +1,15 @@
 #' Use ada-url to parse a url
 #' @param url character. URL to be parsed
+#' @param decode logical. Whether to decode the output (see [utils::URLdecode()]), default to `TRUE`
 #' @return A list of components of the url
 #' @examples
 #' ada_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
-ada_url_parse <- function(url) {
+ada_url_parse <- function(url, decode = TRUE) {
     url <- stringi::stri_enc_toutf8(url)
     url_parsed <- Rcpp_ada_parse(url, nchar(url, type = "bytes"))
-    lapply(url_parsed, URLdecode)
+    if (isTRUE(decode)) {
+      return(lapply(url_parsed, utils::URLdecode))
+    }
+    return(url_parsed)
 }
--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -22,3 +22,8 @@ test_that("works with utf8", {
   res <- ada_url_parse("http://www.müller.de")
   expect_equal(res[["host"]], "www.xn--mller-kva.de")
 })
+
+test_that("URLdecode optional #5",{
+  expect_equal(ada_url_parse("https://www.google.co.jp/search?q=ドイツ")$search, "?q=ドイツ") ## default TRUE
+  expect_equal(ada_url_parse("https://www.google.co.jp/search?q=ドイツ", decode = FALSE)$search, "?q=%E3%83%89%E3%82%A4%E3%83%84")
+})
