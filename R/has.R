44450c52d16c5c033fc24fb1b33ca61cf4f95d2d
schochastics <david@schochastics.net>
2023-09-22 13:13:07 +0200

    actually removed stringi...

--- R/has.R
+++ R/has.R
@@ -5,7 +5,7 @@
 #' ada_has_credentials("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_credentials <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) vapply(url, function(x) Rcpp_ada_has_credentials(x, nchar(x, type = "bytes")), logical(1)), logical(1))
 }
 
@@ -16,7 +16,7 @@ ada_has_credentials <- function(url) {
 #' ada_has_empty_hostname("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_empty_hostname <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_empty_hostname(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -27,7 +27,7 @@ ada_has_empty_hostname <- function(url) {
 #' ada_has_hostname("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_hostname <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_hostname(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -38,7 +38,7 @@ ada_has_hostname <- function(url) {
 #' ada_has_non_empty_username("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_non_empty_username <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_non_empty_username(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -49,7 +49,7 @@ ada_has_non_empty_username <- function(url) {
 #' ada_has_non_empty_password("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_non_empty_password <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_non_empty_password(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -60,7 +60,7 @@ ada_has_non_empty_password <- function(url) {
 #' ada_has_port("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_port <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_port(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -71,7 +71,7 @@ ada_has_port <- function(url) {
 #' ada_has_hash("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_hash <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_hash(x, nchar(x, type = "bytes")), logical(1))
 }
 
@@ -82,6 +82,6 @@ ada_has_hash <- function(url) {
 #' ada_has_search("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_has_search <- function(url) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     vapply(url, function(x) Rcpp_ada_has_search(x, nchar(x, type = "bytes")), logical(1))
 }
