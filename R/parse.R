caef700889952ce64bde5efefa67aa3535448a1c
schochastics <david@schochastics.net>
2023-09-22 13:10:16 +0200

    fix #9

--- R/parse.R
+++ R/parse.R
@@ -6,7 +6,7 @@
 #' ada_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
 #' @export
 ada_url_parse <- function(url, decode = TRUE) {
-    url <- stringi::stri_enc_toutf8(url)
+    url <- utf8::as_utf8(url)
     # url_parsed <- Rcpp_ada_parse(url, nchar(url, type = "bytes"))
     url_parsed <- lapply(url, function(x) Rcpp_ada_parse(x, nchar(x, type = "bytes")))
     if (isTRUE(decode)) {
