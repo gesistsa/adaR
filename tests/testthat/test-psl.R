d421119fc8a0fa984fc6c28d06aaf99532e6735d
schochastics <david@schochastics.net>
2023-09-24 13:29:38 +0200

    fixed tests

--- tests/testthat/test-psl.R
+++ tests/testthat/test-psl.R
@@ -2,12 +2,10 @@ test_that("public_suffix works", {
   urls <- c(
     "https://subsub.sub.domain.co.uk",
     "https://domain.api.gov.uk",
-    "https://thisisnotpart.butthisispartoftheps.kawasaki.jp",
-    "https://a.what.nodebalancer.linode.com"
+    "https://thisisnotpart.butthisispartoftheps.kawasaki.jp"
   )
   ps <- public_suffix(urls)
   expect_equal(ps[1], "co.uk")
-  expect_equal(ps[2], "co.uk")
+  expect_equal(ps[2], "gov.uk")
   expect_equal(ps[3], "butthisispartoftheps.kawasaki.jp")
-  expect_equal(ps[4], "what.nodebalancer.linode.com")
 })
