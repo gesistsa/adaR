af40ec96057d9cbd50c1a89fa6f17e7c30b2a660
schochastics <david@schochastics.net>
2023-09-24 20:53:24 +0200

    value is unsigned int

--- src/urldecode.cpp
+++ src/urldecode.cpp
@@ -22,7 +22,7 @@ CharacterVector url_decode(CharacterVector url) {
         output += input[i];
         i++;
       } else {
-        int value;
+        unsigned int value;
         sscanf(input.substr(i + 1, 2).c_str(), "%x", &value);
         output += static_cast<char>(value);
         i += 3;
