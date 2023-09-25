34314348832e333be1a4d468016db18a54ea7445
chainsawriot <chainsawtiney@gmail.com>
2023-09-25 15:42:37 +0200

    Test all `get` funcions

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,5 +1,6 @@
 test_that("all get functions work", {
     url <- "https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"
+    expect_equal(ada_get_href(url), "https://user_1:password_1@example.org:8080/api?q=1#frag")
     expect_equal(ada_get_username(url), "user_1")
     expect_equal(ada_get_password(url), "password_1")
     expect_equal(ada_get_host(url), "example.org:8080")
@@ -8,10 +9,11 @@ test_that("all get functions work", {
     expect_equal(ada_get_pathname(url), "/api")
     expect_equal(ada_get_search(url), "?q=1")
     expect_equal(ada_get_hash(url), "#frag")
+    expect_equal(ada_get_protocol(url), "https:")
 })
 
-get_functions <- c(ada_get_username, ada_get_password, ada_get_host, ada_get_hostname, ada_get_port, ada_get_pathname,
-                   ada_get_search, ada_get_hash)
+get_functions <- c(ada_get_href, ada_get_username, ada_get_password, ada_get_host, ada_get_hostname, ada_get_port, ada_get_pathname,
+                   ada_get_search, ada_get_hash, ada_get_protocol)
 
 test_that("invalid urls should return NA, #26", {
     url <- "thisisnoturl"
