4c310cc0cb6e7b0ed161d7d4f3f8295319706ecc
chainsawriot <chainsawtiney@gmail.com>
2023-09-25 11:34:52 +0200

    Consolidate doc

--- R/get.R
+++ R/get.R
@@ -9,101 +9,78 @@
     return(out)
 }
 
-#' Get href component of URL
+#' Get a specific component of URL
 #' @inheritParams ada_url_parse
-#' @return logical
+#' @return character, `NA` if not a valid URL
 #' @examples
-#' ada_get_href("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' url <- "https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"
+#' ada_get_href(url)
+#' ada_get_username(url)
+#' ada_get_password(url)
+#' ada_get_port(url)
+#' ada_get_hash(url)
+#' ada_get_host(url)
+#' ada_get_hostname(url)
+#' ada_get_pathname(url)
+#' ada_get_search(url)
+#' ada_get_protocol(url)
+#' ## these functions are vectorized
+#' urls <- c("http://www.google.com", "http://www.google.com:80", "noturl")
+#' ada_get_port(urls)
 #' @export
 ada_get_href <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_href)
 }
 
-#' Get username component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_username("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_username <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_username)
 }
 
-#' Get password component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_password("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_password <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_password)
 }
 
-#' Get port component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_port("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_port <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_port)
 }
 
-#' Get hash component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_hash("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_hash <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_hash)
 }
 
-#' Get host component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_host("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_host <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_host)
 }
 
-#' Get hostname component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_hostname("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_hostname <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_hostname)
 }
 
-#' Get pathname component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_pathname("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_pathname <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_pathname)
 }
 
-#' Get search component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_search("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_search <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_search)
 }
 
-#' Get protocol component of URL
-#' @inheritParams ada_url_parse
-#' @return logical
-#' @examples
-#' ada_get_protocol("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+#' @rdname ada_get_href
 #' @export
 ada_get_protocol <- function(url, decode = TRUE) {
     .get(url, decode, Rcpp_ada_get_protocol)
