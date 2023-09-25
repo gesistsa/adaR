cca3e66e6750ce9dfdd186877a3300e397ee7859
chainsawriot <chainsawtiney@gmail.com>
2023-09-25 19:23:00 +0200

    Update header

--- src/adaR.h
+++ src/adaR.h
@@ -8,26 +8,26 @@ using namespace Rcpp;
 std::string charsub(ada_string stringi);
 
 // cash cow
-Rcpp::DataFrame Rcpp_ada_parse(CharacterVector input_vec, IntegerVector length_vec);
+DataFrame Rcpp_ada_parse(CharacterVector input_vec, IntegerVector length_vec);
 
 // has_*
-bool Rcpp_ada_has_credentials(const char* input, size_t length);
-bool Rcpp_ada_has_empty_hostname(const char* input, size_t length);
-bool Rcpp_ada_has_hostname(const char* input, size_t length);
-bool Rcpp_ada_has_non_empty_username(const char* input, size_t length);
-bool Rcpp_ada_has_non_empty_password(const char* input, size_t length);
-bool Rcpp_ada_has_port(const char* input, size_t length);
-bool Rcpp_ada_has_hash(const char* input, size_t length);
-bool Rcpp_ada_has_search(const char* input, size_t length);
+LogicalVector Rcpp_ada_has_credentials(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_empty_hostname(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_hostname(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_non_empty_username(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_non_empty_password(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_port(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_hash(const CharacterVector& url_vec);
+LogicalVector Rcpp_ada_has_search(const CharacterVector& url_vec);
 
 // get_*
-CharacterVector Rcpp_ada_get_href(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_username(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_password(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_port(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_hash(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_host(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_hostname(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_pathname(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_search(CharacterVector input_vec, IntegerVector length_vec);
-CharacterVector Rcpp_ada_get_protocol(CharacterVector input_vec, IntegerVector length_vec);
+CharacterVector Rcpp_ada_get_href(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_username(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_password(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_port(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_hash(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_host(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_hostname(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_pathname(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_search(const CharacterVector& url_vec);
+CharacterVector Rcpp_ada_get_protocol(const CharacterVector& url_vec);
