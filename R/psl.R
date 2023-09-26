03cb768f50483d42ffbe3abc9d6722447d4ab309
schochastics <david@schochastics.net>
2023-09-26 23:23:04 +0200

    fix #44

--- R/psl.R
+++ R/psl.R
@@ -9,9 +9,17 @@ public_suffix <- function(url) {
     suffix_match <- triebeard::longest_match(adaR_env$trie_ps, url_reverse(url))
     with_wildcard <- suffix_match %in% psl$wildcard
     if (any(with_wildcard)) {
-        pat <- paste0("\\.", suffix_match[with_wildcard], "$")
-        dom <- mapply(function(x, y) sub(x, "", y), pat, url[with_wildcard], USE.NAMES = FALSE)
-        suffix_match[with_wildcard] <- paste0(sub(".*\\.([^\\.]+)$", "\\1", dom), ".", suffix_match[with_wildcard])
+        host <- ada_get_hostname(url[with_wildcard])
+        idx <- !(host == suffix_match[with_wildcard])
+        pat <- paste0("\\.", suffix_match[with_wildcard][idx], "$")
+        dom <- mapply(function(x, y) sub(x, "", y), pat, url[with_wildcard][idx], USE.NAMES = FALSE)
+        idy <- .has_dot(dom)
+        suffix_match[with_wildcard][idx][idy] <- paste0(sub(".*\\.([^\\.]+)$", "\\1", dom[idy]), ".", suffix_match[with_wildcard][idx][idy])
+        suffix_match[with_wildcard][idx][!idy] <- host[with_wildcard][idx][!idy]
     }
     suffix_match
+}
+
+.has_dot <- function(x) {
+    grepl("\\.", x)
 }
