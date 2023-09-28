7c22810f1a84c98a0ce74198cc09bbcf004a80cd
schochastics <david@schochastics.net>
2023-09-28 20:58:51 +0200

    better doc for public_suffix() #54

--- R/psl.R
+++ R/psl.R
@@ -1,18 +1,26 @@
-#' Extract the public suffix from a vector of domains
+#' Extract the public suffix from a vector of domains or hostnames
 #'
-#' @inheritParams ada_url_parse
+#' @param domains character. vector of domains or hostnames
 #' @export
-public_suffix <- function(url) {
-    if (is.null(url)) {
+#' @examples
+#' public_suffix("http://example.com")
+#'
+#' # doesn't work for general URLs
+#' public_suffix("http://example.com/path/to/file")
+#'
+#' # extracting hostname first does the trick
+#' public_suffix(ada_get_hostname("http://example.com/path/to/file"))
+public_suffix <- function(domains) {
+    if (is.null(domains)) {
         return(character())
     }
-    suffix_match <- triebeard::longest_match(adaR_env$trie_ps, url_reverse(url))
+    suffix_match <- triebeard::longest_match(adaR_env$trie_ps, url_reverse(domains))
     with_wildcard <- suffix_match %in% psl$wildcard
     if (any(with_wildcard)) {
-        host <- ada_get_hostname(url[with_wildcard])
+        host <- ada_get_hostname(domains[with_wildcard])
         idx <- !(host == suffix_match[with_wildcard])
         pat <- paste0("\\.", suffix_match[with_wildcard][idx], "$")
-        dom <- mapply(function(x, y) sub(x, "", y), pat, url[with_wildcard][idx], USE.NAMES = FALSE)
+        dom <- mapply(function(x, y) sub(x, "", y), pat, domains[with_wildcard][idx], USE.NAMES = FALSE)
         idy <- .has_dot(dom)
         suffix_match[with_wildcard][idx][idy] <- paste0(sub(".*\\.([^\\.]+)$", "\\1", dom[idy]), ".", suffix_match[with_wildcard][idx][idy])
         suffix_match[with_wildcard][idx][!idy] <- host[idx][!idy]
